dcfcec80563c3facd5f61c33b5e3141505195b4d
Wendel Cutrim <wendelcutrim@hotmail.com>
2021-07-06 22:11:01 -0300

    Criado servidor e estabelecido as rotas Home, Contato e manutenção. Também foi criado o arquivo html da página de manutenção. Criado as pastas public, views, arquivos gitignore e instalado o express.

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => res.send('Seja bem vindo'));
+app.get('/contato', (req,res) => res.send('Contatos'));
+app.get('/manutencao', (req,res) => res.sendFile(__dirname + "/views/manutencao.html"));
+
+//subindo o servidor
+app.listen(3000, () => console.log("Servidor está funcionando"));
