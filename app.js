cab76d575a35a7b5dbf71a8955394979d9c187bb
Wendel Cutrim <wendelcutrim@hotmail.com>
2021-07-08 01:46:28 -0300

    Adicionado a constante path, para trabalhar com o método .sendFile() do express futuramente para definir as rotas de envio de arquivos do servidor.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.get('/', (req,res) => res.send('Seja bem vindo'));
 app.get('/contato', (req,res) => res.send('Contatos'));
