859dd0221d923323636f7f7c4ae0b84acca67068
Wendel Cutrim <wendelcutrim@hotmail.com>
2021-07-08 22:12:49 -0300

    Criado arquivo do blog, alterado o package.json na seção script para dar o npm start no nodemon app.js, adicionado a pasta de imagens na public e inserido o comando de acordo com a documentação do express para ler imagens da pasta public.

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/', (req,res) => res.send('Seja bem vindo'));
+app.use(express.static('public'));
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname + "/views/home.html")));
+
+app.get('/home',(req,res)=>res.sendFile(path.join(__dirname + "/views/home.html")));
+
 app.get('/contato', (req,res) => res.send('Contatos'));
-app.get('/manutencao', (req,res) => res.sendFile(__dirname + "/views/manutencao.html"));
+
+app.get('/manutencao', (req,res) => res.sendFile(path.join(__dirname + "/views/manutencao.html")));
+
+app.get('/blog', (req,res) => res.sendFile(path.join(__dirname + "/views/blog.html")));
 
 //subindo o servidor
 app.listen(3000, () => console.log("Servidor está funcionando"));
+
+//criar arquivo blog
