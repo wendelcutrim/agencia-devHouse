d7f0ec5011b016e280f9bcefc12612d66d1cea1b
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-02-20 21:25:00 -0300

    Realizado a revisão da aula 24/08/2021 Middlewares - Multer, inserido alguns comentários no middleware de upload e algumas correçãoes na rota de produtos

--- middlewares/uploads.js
+++ middlewares/uploads.js
@@ -4,13 +4,14 @@ const { v4: gerarNomeAleatorio } = require('uuid');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
+      //Passando o caminho para onde os uploads serão salvos.
       cb(null, path.resolve("public", "uploads"));
     },
     filename: function (req, file, cb) {
         //Reconhecendo o formato do arquivo.
         const extensao = file.originalname.split('.').pop();
         const nomeArquivo = `${gerarNomeAleatorio()}.${extensao}`;
-        //Outro método seria: const nomeArquivo = gerarNomeAletoario() + '.' + extensao
+        //Acima está utilizando template string, onde tudo que está dentro das crases, será transformado em string, para inserir uma variável, basta colocar o nome da váriavel no seguinte formato: ${varName}. Outro método seria: const nomeArquivo = gerarNomeAletoario() + '.' + extensao
 
         cb(null, nomeArquivo);
     },
